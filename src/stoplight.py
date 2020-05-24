781072a1229f7ee9b9cc7abc537fe744dd518e54
Mike Urbanski <mikeurbanski1@gmail.com>
2020-05-24 17:23:38 -0500

    make interactive

--- src/stoplight.py
+++ src/stoplight.py
@@ -8,12 +8,12 @@ GPIO.setmode(GPIO.BCM)
 red = int(sys.argv[1])
 yellow = int(sys.argv[2])
 green = int(sys.argv[3])
-pins = [red, yellow, green]
+pins = {'r': red, 'y': yellow, 'g': green}
 
 
 def signal_handler(sig=None, frame=None):
     print('You pressed Ctrl+C!')
-    for pin in pins:
+    for pin in pins.values():
         GPIO.output(pin, 0)
     GPIO.cleanup()
     sys.exit(0)
@@ -25,17 +25,29 @@ for pin in pins:
     GPIO.setup(pin, GPIO.OUT)
     GPIO.output(pin, 0)
 
+states = {'r': 0, 'y': 0, 'g': 0}
+
+
 try:
     while True:
-        GPIO.output(red, 1)
-        sleep(2)
-        GPIO.output(red, 0)
-        GPIO.output(yellow, 1)
-        sleep(1)
-        GPIO.output(yellow, 0)
-        GPIO.output(green, 1)
-        sleep(2)
-        GPIO.output(green, 0)
+        line = sys.stdin.readline()
+        parts = line.split(' ')
+        for p in parts:
+            if p not in ['r', 'y', 'g']:
+                print(f'Ignoring: {p}')
+            else:
+                new_val = 1 - states[p]
+                GPIO.output(pins[p], new_val)
+                states[p] = new_val
+        # GPIO.output(red, 1)
+        # sleep(2)
+        # GPIO.output(red, 0)
+        # GPIO.output(yellow, 1)
+        # sleep(1)
+        # GPIO.output(yellow, 0)
+        # GPIO.output(green, 1)
+        # sleep(2)
+        # GPIO.output(green, 0)
 
 except Exception as e:
     print(f"Error: {e}")
